58178483cf82950692b7e828d2cc56cb3dea6c75
Marco <marco.steffensky@gmail.com>
2017-09-25 13:25:47 +0200

    Change secret files

--- app/services/weather_service/endpoint.rb
+++ app/services/weather_service/endpoint.rb
@@ -5,7 +5,7 @@ module WeatherService
     base_uri 'api.openweathermap.org/data'
 
     def initialize(lat, lng)
-      api_key = Rails.application.secrets.weather_key
+      api_key = Rails.env.production? ? Rails.application.secrets.weather_key : Rails.application.secrets_copy.weather_key
       @options = { query: { lat: lat, lon: lng, appid: api_key, units: "metric", cnt: "7" } }
     end
 
